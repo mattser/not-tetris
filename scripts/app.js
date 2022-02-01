9304aa96c73fecfe4d5685d7c3f79da4e93f3f08
mattser <jomatser@gmail.com>
2022-02-01 15:18:58 +0000

    Finally rendered terrible grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,74 @@
-// coordinates are y,x
+// Call things with x,y 
+// Coordinates will be in a 3 letter string: 2 for y (0-21) and 1 for x (0-9)
 
+// New Game
 class Game {
-  constructor (board) {
-    this._board = board;
+  constructor (element) {
+    this._board = new Board(element);
+    this._element = element;
     this._score = 0;
+    this.addKeyPressListener();
+    this._activeTetromino = {};
   }
-  get board() {
+  get board () {
     return this._board;
+  }
+
+  addKeyPressListener() {
+    document.addEventListener('keypress',(event) => {
+      if (event.code === "Space") {
+        this._activeTetromino = new Tetromino(Math.floor(Math.random()*9));
+        console.log("You Pressed Space")
+      }
+    })
   }
 }
 
 class Board {
   constructor (element) {
     this._element = element;
-    this._board = this.constructBoard()
+    this.drawNewBoard(element);
+    this._grid = this.createGrid();
   }
-  get element() {
-    return this._element;
+
+  drawNewBoard (element) {
+    for (let y = 0; y <= 21; y++) {
+      for (let x = 0; x <=9; x++) {
+        element.innerHTML+=`\n<div class="board__block" id="${String(x)+"x"+String(y)}">${String(x)+"x"+String(y)}</div>`;
+      }
+    }
   }
-  constructBoard() {
-    return new Array(22).fill(new Array(10).fill(0));
+  createGrid () {
+    return new Array(10).fill( new Array(22).fill(0));
   }
-  get board() {
-    return this._board;
+
+  get grid () {
+    return this._grid;
   }
-  set board(newBoard) {
-    this._board = newBoard
-  }
-  changeBoard(y,x,value) {
-    board[y][x] = value;
+  set grid (value) {
+    this._grid = value;
   }
 }
+// Create a new board
+  // Board is 10 x 22 cells
+  // Draw grid on the html
 
+class Tetromino {
+  constructor (spawnPos) {
+    this._types = ["L","T","Square","Pipe","Skew"];
+    this._type = this._types[Math.floor(Math.random()*this._types.length)];
+    this._centralPos = spawnPos;
+    console.log(this._type);
+  }
 
+}
 
-const board = new Board( document.querySelector(".board"));
-const game = new Game(board);
+// Create New Tetromino
+  // Pick random from a pool
+  // Render at the top of the pag
+
+// Move Tetromino Down
+  // On click, translate the position of the tetromino down by 1.
+  // Tetrominos are rended from a single position and orientation parameter
+
+const notTetris = new Game(document.querySelector(".board"));
